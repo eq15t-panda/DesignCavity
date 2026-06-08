65eb0813e73ea2357976edc16784ba8d4f6db790
Chris Ze Third <william.genetelli@gmail.com>
2026-06-08 09:35:51 +0200

    Create new branch, add new functions, and simplify the code.

--- abcd.py
+++ abcd.py
@@ -1,12 +1,11 @@
-
 import numpy as np
-
 
 
 # ABCD matrix - Boyd Table 4.1
 def M_free(d):
     return np.array([[1, d],
                      [0, 1]])
+
 
 def M_sph_mirror(R):
     return np.array([[1, 0],
@@ -15,13 +14,11 @@ def M_sph_mirror(R):
 
 def stability_condition(A,D):
     """ stability condition resonators -1 < (A+D)/2 <1 """
-    return -1 <  (A+D)/2   <  1
+    return -1 < (A + D)/2 < 1
 
 
-
-
-### Concave Concave cavity 
-def concave_concave( roc1, roc2, L_values, wavelength, d= 10e-3, n=1.816   ): 
+# Concave Concave cavity
+def concave_concave( roc1, roc2, L_values, wavelength, d= 10e-3, n=1.816   ):
     """
     Parameters
         roc1, roc2 : float - Radius of curvature of mirror 1 and mirror 2
@@ -38,11 +35,11 @@ def concave_concave( roc1, roc2, L_values, wavelength, d= 10e-3, n=1.816   ):
     stability_values = np.zeros(L_values.shape, dtype=bool)
     waist_values = []
     z_waist_values = []
-    
+
     for i, L in enumerate(L_values):
 
         # Propagation matrix 
-        L_air = L - d    
+        L_air = L - d
         L1 = L_air / 2
         L2 = L_air / 2
 
@@ -58,21 +55,21 @@ def concave_concave( roc1, roc2, L_values, wavelength, d= 10e-3, n=1.816   ):
         stability_values[i] = stability_condition(A,D)
 
         if stability_values[i]:
-    
+
             q_roots = np.roots([C, D - A, -B])
             q0 = q_roots[np.imag(q_roots) > 0][0]
 
             z_array = np.linspace(0, L, 1000)
-            q_out = q0 + z_array  
-            
+            q_out = q0 + z_array
+
             w_array = np.sqrt(wavelength / (np.pi * np.imag( -1 / q_out)))
 
             i0 = np.argmin(w_array)
 
-        
+
             waist_values.append(w_array[i0] )   # m
-            z_waist_values.append(z_array[i0] )    # m 
-            
+            z_waist_values.append(z_array[i0] )    # m
+
     return np.array(waist_values), np.array(z_waist_values),  stability_values
 
 
@@ -80,7 +77,7 @@ def concave_concave( roc1, roc2, L_values, wavelength, d= 10e-3, n=1.816   ):
 # Kogelnik, Laser beams and resonators, Table II
 def kogelnik_stability_values( R1, R2, L_values):
     stability_values = np.zeros(L_values.shape, dtype=bool)
-    
+
     for i, L in enumerate(L_values):
 
         propagation_matrix =  M_sph_mirror(R1) @ M_free(L) @ M_sph_mirror(R2)  @ M_free(L)
@@ -93,10 +90,10 @@ def kogelnik_stability_values( R1, R2, L_values):
     return stability_values
 
 
-def kogelnik_waist_concave_concave(R1, R2, L, wavelength): 
+def kogelnik_waist_concave_concave(R1, R2, L, wavelength):
 
     x =  np.sqrt(L*(R1-L)*(R2-L)*(R1+R2-L))  / np.abs(R1+R2 -2*L)  # = b/2
-    waist_carre = wavelength * x / np.pi 
+    waist_carre = wavelength * x / np.pi
     waist = np.sqrt( waist_carre )
     return waist
 
@@ -104,7 +101,7 @@ def kogelnik_waist_concave_concave(R1, R2, L, wavelength):
 
 
 ### Plan Concave
-def plan_concave( roc1, L_values, wavelength,  wz_target, beam_profile = False): 
+def plan_concave( roc1, L_values, wavelength,  wz_target, beam_profile = False):
 
     stability_values = np.zeros(L_values.shape, dtype=bool)
 
@@ -112,10 +109,10 @@ def plan_concave( roc1, L_values, wavelength,  wz_target, beam_profile = False):
     wz_close_to_taget_values = []
     z_close_to_taget_values = []
     i_target = None
-    
+
     wz__lenths_array = []
     z__lenths_array = []
-    
+
     for i, L in enumerate(L_values):
 
         # Propagation matrix 
@@ -137,14 +134,14 @@ def plan_concave( roc1, L_values, wavelength,  wz_target, beam_profile = False):
             waist_values.append(w0 * 1e6)
 
             # w(z) target value given in the fonction in m
-            q_out = q0 + z_array  
+            q_out = q0 + z_array
             w_array = np.sqrt(wavelength / (np.pi * np.imag( -1 / q_out)))
 
             i_target = np.argmin(np.abs(w_array - wz_target))
             wz_close_to_taget_values.append( w_array[i_target])
             z_close_to_taget_values.append( z_array[i_target])
 
-            if beam_profile: 
+            if beam_profile:
                 wz__lenths_array.append(np.array(w_array))
                 z__lenths_array.append(np.array(z_array))
 
@@ -154,7 +151,7 @@ def plan_concave( roc1, L_values, wavelength,  wz_target, beam_profile = False):
 
 
 ## Plan Concave
-def plan_concave_cristal( roc1, L_values, wavelength, d, n, wz_target, beam_profile = False): 
+def plan_concave_cristal( roc1, L_values, wavelength, d, n, wz_target, beam_profile = False):
 
     stability_values = np.zeros(L_values.shape, dtype=bool)
 
@@ -162,13 +159,13 @@ def plan_concave_cristal( roc1, L_values, wavelength, d, n, wz_target, beam_prof
     wz_close_to_taget_values = []
     z_close_to_taget_values = []
     i_target = None
-    
+
     wz__lenths_array = []
     z__lenths_array = []
-    
+
     for i, L in enumerate(L_values):
 
-        L_air = L - d    
+        L_air = L - d
         # Propagation matrix 
         M = ( M_free(d / n) @ M_free(L_air) @ M_sph_mirror(roc1) @ M_free(L_air) @ M_free(d / n) )
         A = M[0, 0]
@@ -188,14 +185,14 @@ def plan_concave_cristal( roc1, L_values, wavelength, d, n, wz_target, beam_prof
             waist_values.append(w0 * 1e6)
 
             # w(z) target value given in the fonction in m
-            q_out = q0 + z_array  
+            q_out = q0 + z_array
             w_array = np.sqrt(wavelength / (np.pi * np.imag( -1 / q_out)))
 
             i_target = np.argmin(np.abs(w_array - wz_target))
             wz_close_to_taget_values.append( w_array[i_target])
             z_close_to_taget_values.append( z_array[i_target])
 
-            if beam_profile: 
+            if beam_profile:
                 wz__lenths_array.append(np.array(w_array))
                 z__lenths_array.append(np.array(z_array))
 
